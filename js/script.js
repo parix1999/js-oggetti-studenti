68479fed93e6f405010e055bffeb57a61a33bc07
parix1999 <parisottoedoardo@gmail.com>
2021-06-15 19:03:51 +0200

    js-3.0

--- js/script.js
+++ js/script.js
@@ -9,11 +9,27 @@ var studente = {
 // key : valore,
     name : 'Edoardo',
     surname : 'Parisotto',
-    age : '21'
+    age : '21',
+    // Aggiunta della array per la parte 2 con all'interno gli studenti che andranno in stampa video:
+    students : [
+        nome = 'Gianni', 'Vercingitore', 'Lello',
+        cognome = 'Ciano', 'Vessato', 'Lillo',
+    ]
 }
 
 // Ora mi serve un for in per stampare i vari oggetti contenuti nella var stundente.
 for (var key in studente) {
     console.log(key, studente[key]);
     output1.innerHTML += '<li>' +  key + ' ' +studente[key] + '</li>'
+}
+
+/* PARTE 2:
+Creare un array di oggetti di studenti. 
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.  
+Ricordatevi, è un array, quindi? for.
+*/
+
+// Ora si cicla su ogno studente all'interno della array:
+for (var i = 0; i < studente.students.length; i++) {
+    console.log(studente.students[i])
 }
