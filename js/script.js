7a1f34bd1e980a5d8d739b75102dd311a7f1ff2f
parix1999 <parisottoedoardo@gmail.com>
2021-06-15 18:44:33 +0200

    js-1.0

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* PARTE 1:
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: 
+nome, cognome e età. Stampare a schermo attraverso un ciclo for-in tutte 
+le proprietà dell’oggetto.
+*/
+
+// Creo la variabile con gli oggetti (key e valore):
+var studente = {
+    name : 'Edoardo',
+    surname : 'Parisotto',
+    age : '21'
+}
+
