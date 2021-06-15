fd61e4e8e6a686af2c317ed923d2d638c2062fcf
parix1999 <parisottoedoardo@gmail.com>
2021-06-15 19:30:44 +0200

    js-4.0

--- js/script.js
+++ js/script.js
@@ -10,17 +10,12 @@ var studente = {
     name : 'Edoardo',
     surname : 'Parisotto',
     age : '21',
-    // Aggiunta della array per la parte 2 con all'interno gli studenti che andranno in stampa video:
-    students : [
-        nome = 'Gianni', 'Vercingitore', 'Lello',
-        cognome = 'Ciano', 'Vessato', 'Lillo',
-    ]
 }
 
 // Ora mi serve un for in per stampare i vari oggetti contenuti nella var stundente.
 for (var key in studente) {
     console.log(key, studente[key]);
-    output1.innerHTML += '<li>' +  key + ' ' +studente[key] + '</li>'
+    output1.innerHTML += '<li>' +  key + ' ' +studente[key] + '</li>';
 }
 
 /* PARTE 2:
@@ -28,8 +23,25 @@ Creare un array di oggetti di studenti.
 Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.  
 Ricordatevi, è un array, quindi? for.
 */
+var output2 = document.getElementById('result2');
+
+// Aggiunta della array per la parte 2 con all'interno gli studenti che andranno in stampa video:
+var classe = {
+studentsName : ['Gianni','Lillo','Lello'],
+studentsSurname : ['Ciano','Vessato','Bangi']
+}
 
 // Ora si cicla su ogno studente all'interno della array:
-for (var i = 0; i < studente.students.length; i++) {
-    console.log(studente.students[i])
+for (var i = 0; i < classe.studentsName.length; i++) {
+    // e si stampa l'indice ciclato all'interno dell'array per tirar fuori i nomi
+    console.log(classe.studentsName[i])
+    output2.innerHTML += '<li>' + classe.studentsName[i] + '</li>'; 
+      
+}
+
+for (var x = 0; x < classe.studentsSurname.length; x++) {
+    // e si stampa l'indice ciclato all'interno dell'array per tirar fuori i cognomi:
+    console.log(classe.studentsSurname[x])
+    output2.innerHTML += '<li>' + classe.studentsSurname[x] + '</li>';
+    
 }
